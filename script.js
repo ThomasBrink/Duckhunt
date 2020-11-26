daa25a030ac426d8c4bafe38b47907823d6962eb
Thomas <99062905@mydavinci.nl>
2020-11-26 07:45:06 +0100

    test

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
 var eend = document.getElementById("eend");
 var button_1 = document.getElementById("button_1");
+var button_2 = document.getElementById("button_2");
+var button_3 = document.getElementById("button_3");
+
+var pixelsO = 0;
+var pixelsN = 0;
+var pixelsZ = 0;
+var pixelsW = 0;
+
 
 button_1.addEventListener("click", function(){
 	flyO();
 });
 
+button_2.addEventListener("click", function(){
+	flyN();
+});
+
+button_3.addEventListener("click", function(){
+	flyZ();
+});
+
 function flyO(){
-	document.getElementById("eend").style.marginTop = "75px"
+	for(i=0; i<75; i++){
+		pixelsO++
+	}
+	eend.style.marginLeft = pixelsO + "px"
+}
+
+function flyN(){
+	for(i=0; i<75; i++){
+		pixelsN++
+	}
+	eend.style.marginBottom = pixelsN + "px"
+}
+
+function flyZ(){
+	for(i=0; i<75; i++){
+		pixelsZ++
+	}
+	eend.style.marginTop =  pixelsZ + "px"
 }
 
 function flyW(){
-	document.getElementById("eend").style.marginRight = "75px"
+	for(i=0; i<75; i++){
+		pixelsW++
+	}
+	eend.style.marginRight = pixelsW + "px"
 }
+
+
+
+
+
 
 
