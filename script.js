798b3e9b26958d916488e3ff9ffe13f1acd70c2a
Thomas <99062905@mydavinci.nl>
2020-11-26 08:13:14 +0100

    oof

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var eend = document.getElementById("eend");
 var button_1 = document.getElementById("button_1");
 var button_2 = document.getElementById("button_2");
 var button_3 = document.getElementById("button_3");
+var button_4 = document.getElementById("button_4");
+
 
 var pixelsO = 0;
 var pixelsN = 0;
@@ -21,33 +23,38 @@ button_3.addEventListener("click", function(){
 	flyZ();
 });
 
+button_4.addEventListener("click", function(){
+	flyW();
+});
+
 function flyO(){
 	for(i=0; i<75; i++){
 		pixelsO++
 	}
-	eend.style.marginLeft = pixelsO + "px"
+	eend.style.left = pixelsO + "px"
 }
 
 function flyN(){
 	for(i=0; i<75; i++){
 		pixelsN++
 	}
-	eend.style.marginBottom = pixelsN + "px"
+	eend.style.bottom = pixelsN + "px"
 }
 
 function flyZ(){
 	for(i=0; i<75; i++){
 		pixelsZ++
 	}
-	eend.style.marginTop =  pixelsZ + "px"
+	eend.style.top =  pixelsZ + "px"
 }
 
 function flyW(){
 	for(i=0; i<75; i++){
 		pixelsW++
 	}
-	eend.style.marginRight = pixelsW + "px"
+	eend.style.right = pixelsW + "px"
 }
+
 
 
 
