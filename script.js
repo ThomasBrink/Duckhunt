c53f21c3adaa15c11cf54b6753de47a53c3202fc
Thomas <99062905@mydavinci.nl>
2020-11-30 11:52:25 +0100

    random movement

--- script.js
+++ script.js
@@ -1,59 +1,95 @@
-var eend = document.getElementById("eend");
-var button_1 = document.getElementById("button_1");
-var button_2 = document.getElementById("button_2");
-var button_3 = document.getElementById("button_3");
-var button_4 = document.getElementById("button_4");
+var richting = ["left", "richt", "up", "down"];
+var eend = document.getElementById("eend")
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+var up = document.getElementById("up");
+var down = document.getElementById("down");
+var PosLeft = 670;
+var PosTop = 270;
+var random
 
 
-var pixelsO = 0;
-var pixelsN = 0;
-var pixelsZ = 0;
-var pixelsW = 0;
-
-
-button_1.addEventListener("click", function(){
-	flyO();
-});
-
-button_2.addEventListener("click", function(){
-	flyN();
-});
-
-button_3.addEventListener("click", function(){
-	flyZ();
-});
-
-button_4.addEventListener("click", function(){
-	flyW();
-});
-
-function flyO(){
-	for(i=0; i<75; i++){
-		pixelsO++
+setInterval(function(){ 
+	 random = Math.floor(Math.random() * 4) + 0;
+	 console.log(random)
+	if(random == 0){
+		moveleft();
 	}
-	eend.style.left = pixelsO + "px"
+	else if(random == 1){
+		moveright();
+	}
+
+	else if(random == 2){
+		moveup();
+	}
+	else if(random == 3){
+		movedown();
+	}
+		}, 1000);
+
+function moveduck(richting){
+	if(richting == "left"){
+		PosLeft -= 75;
+		eend.style.left = PosLeft + "px";
+	}
+
+	else if(richting == "right"){
+		PosLeft += 75;
+		eend.style.left = PosLeft + "px";
+	}
+
+	else if(richting == "up"){
+		PosTop -= 75;
+		eend.style.top = PosTop + "px";
+	}
+	else{
+		PosTop += 75;
+		eend.style.top = PosTop + "px";
+	}
+
 }
 
-function flyN(){
-	for(i=0; i<75; i++){
-		pixelsN++
-	}
-	eend.style.bottom = pixelsN + "px"
+function moveleft(){
+	PosLeft -= 75;
+	eend.style.left = PosLeft + "px";
 }
 
-function flyZ(){
-	for(i=0; i<75; i++){
-		pixelsZ++
-	}
-	eend.style.top =  pixelsZ + "px"
+function moveright(){
+	PosLeft += 75;
+	eend.style.left = PosLeft + "px";
 }
 
-function flyW(){
-	for(i=0; i<75; i++){
-		pixelsW++
-	}
-	eend.style.right = pixelsW + "px"
+function moveup(){
+	PosTop -= 75;
+	eend.style.top = PosTop + "px";
 }
+
+function movedown(){
+	PosTop += 75;
+	eend.style.top = PosTop + "px";
+}
+
+
+left.addEventListener("click", function(){
+	PosLeft -= 75;
+	eend.style.left = PosLeft + "px";
+});
+
+right.addEventListener("click", function(){
+	PosLeft += 75;
+	eend.style.left = PosLeft + "px";
+});
+
+up.addEventListener("click", function(){
+	PosTop -= 75;
+	eend.style.top = PosTop + "px";
+});
+
+down.addEventListener("click", function(){
+	PosTop += 75;
+	eend.style.top = PosTop + "px";
+});
+
 
 
 
