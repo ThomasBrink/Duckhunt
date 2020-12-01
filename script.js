4051ae82813f79a80b06e91ab7854246a83ba2db
Thomas <99062905@mydavinci.nl>
2020-12-01 08:35:38 +0100

    Schuinen richtingen toegeveogd

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-var richting = ["left", "right", "up", "down"];
+var richting = ["W", "O", "N", "Z", "NO", "ZO", "ZW", "NW" ];
 var eend = document.getElementById("eend")
-var left = document.getElementById("left");
-var right = document.getElementById("right");
-var up = document.getElementById("up");
-var down = document.getElementById("down");
 var hit = document.getElementById("hit");
 var miss = document.getElementById("miss");
 var container = document.getElementById("container");
@@ -15,48 +11,97 @@ var miss = 0;
 
 
 setInterval(function(){ 
-	 random = Math.floor(Math.random() * 4) + 0;
+	 random = Math.floor(Math.random() * 8) + 0;
+	 console.log(random)
 	 var number = richting[random];
 	 moveduck(number);
 		}, 1000);
 
 function moveduck(richting){
-	if(richting == "left"){
+	if(richting == "W"){
 		if(PosLeft < 1){
 			PosLeft = PosLeft - 0;
 		}
 		else{
-		PosLeft -= 75;
-		eend.style.left = PosLeft + "px";
+			PosLeft -= 75;
+			eend.style.left = PosLeft + "px";
 		}
 	}
 
-	else if(richting == "right"){
+	else if(richting == "O"){
 		if(PosLeft > 1350){
 			PosLeft = PosLeft - 0;
 		}
 		else{
-		PosLeft += 75;
-		eend.style.left = PosLeft + "px";
+			PosLeft += 75;
+			eend.style.left = PosLeft + "px";
 		}
 	}
 
-	else if(richting == "up"){
+	else if(richting == "N"){
 		if(PosTop < 1){
 			PosTop = PosTop - 0;
 		}
 		else{
-		PosTop -= 75;
-		eend.style.top = PosTop + "px";
+			PosTop -= 75;
+			eend.style.top = PosTop + "px";
 		}
 	}
-	else{
+
+	else if(richting == "Z"){
 		if(PosTop > 495){
 			PosTop = PosTop - 0;
 		}
 		else{
-		PosTop += 75;
-		eend.style.top = PosTop + "px";
+			PosTop += 75;
+			eend.style.top = PosTop + "px";
+		}
+	}
+
+	else if(richting == "NO"){
+		if(PosTop < 1 || PosLeft > 1350){
+			PosTop = PosTop - 0;
+		}
+		else{
+			PosTop -= 75;
+			eend.style.top = PosTop + "px";
+			PosLeft += 75;
+			eend.style.left = PosLeft + "px";
+		}
+	}
+
+	else if(richting == "ZO"){
+		if(PosLeft > 1350 || PosTop > 495){
+			PosTop = PosTop - 0;
+		}
+		else{
+			PosLeft += 75;
+			eend.style.left = PosLeft + "px";
+			PosTop += 75;
+			eend.style.top = PosTop + "px";
+		}
+	}
+
+	else if(richting == "ZW"){
+		if(PosTop > 495 || PosLeft < 1){
+			PosTop = PosTop - 0;
+		}
+		else{
+			PosTop += 75;
+			eend.style.top = PosTop + "px";
+			PosLeft -= 75;
+			eend.style.left = PosLeft + "px";
+		}
+	}
+	else if(richting == "NW"){
+		if(PosTop < 1){
+			PosTop = PosTop - 0;
+		}
+		else{
+			PosTop -= 75;
+			eend.style.top = PosTop + "px";
+			PosLeft -= 75;
+			eend.style.left = PosLeft + "px";
 		}
 	}
 
